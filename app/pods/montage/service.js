853fd3bed78c305fae65c7d47c19c422c87096ee
Jamie Burkart <jamie.burkart@gmail.com>
2020-08-31 04:01:50 -0500

    player is playing story

--- app/pods/montage/service.js
+++ app/pods/montage/service.js
@@ -10,25 +10,13 @@ export default Service.extend({
   allPlans: alias('planner.allPlans'),
 
   // init
-  planIndex: 0,
-  stepIndex: 0,
-  propositionIndex: 0,
-  clipIndex: 0,
-  hasPlayed: false,
+  planIndex: null,
+  playing: false,
   init() {
     this._super(...arguments);
     this.set('planIndex', 0);
-    this.set('stepIndex', 0);
-    this.set('propositionIndex', 0);
-    this.set('clipIndex', 0);
+    this.set('playing', false);
   },
-
-  // actions
-  playing() {
-    this.set('hasPlayed', true);
-  },
-  nextProposition() {},
-  // queue management
   currentPlan: computed('allPlans', 'planIndex', function () {
     let allPlans = this.get('allPlans');
     if (isPresent(allPlans)) {
@@ -36,40 +24,77 @@ export default Service.extend({
     }
     return null;
   }),
-  currentStep: computed('currentPlan', 'stepIndex', function () {
-    let currentPlan = this.get('currentPlan');
-    if (isPresent(currentPlan)) {
-      return currentPlan[this.get('stepIndex') % currentPlan.length];
-    }
-    return null;
-  }),
-  currentPropositions: alias('currentStep.newAdditions'),
-  currentProposition: computed(
-    'currentPropositions',
-    'propositionIndex',
-    function () {
-      let currentPropositions = this.get('currentPropositions');
-      if (isPresent(currentPropositions)) {
-        return currentPropositions[
-          this.get('propositionIndex') % currentPropositions.length
-        ];
-      }
-      return null;
-    }
-  ),
-  currentClips: alias('currentProposition.clips'),
-  currentClip: computed('currentClips', 'clipIndex', function () {
-    let currentClips = this.get('currentClips');
-    if (isPresent(currentClips)) {
-      let currentClip = currentClips.objectAt(
-        this.get('clipIndex') % currentClips.get('length')
-      );
-      log(currentClip);
-      return currentClip;
-    }
-    return null;
-  }),
 
+  // actions
+  video: null,
+  currentClip: null,
+  currentOperator: null,
+  currentClips: null,
   currentClipsSorting: Object.freeze(['playCount', 'position']),
   currentClipsSorted: sort('currentClips', 'currentClipsSorting'),
+
+  incrementPlan() {
+    // increment plan on each play
+    let planIndex = this.get('planIndex');
+    if (isPresent(planIndex)) {
+      planIndex = planIndex + 1;
+    } else {
+      planIndex = 0;
+    }
+    this.set('planIndex', planIndex);
+  },
+
+  async clipEnded() {
+    let vid = this.get('video');
+    return new Promise(function (resolve) {
+      vid.addEventListener('ended', () => {
+        resolve();
+      });
+    });
+  },
+
+  async process(plan) {
+    let vid = this.get('video');
+    for (const step of plan) {
+      let { newAdditions, operator } = step;
+      if (isPresent(operator) && isPresent(operator.get('name'))) {
+        // update graph focus
+        this.set('currentOperator', operator);
+      }
+      if (isPresent(newAdditions)) {
+        for (const proposition of newAdditions) {
+          let clips = proposition.get('clips');
+          if (isPresent(clips)) {
+            this.set('currentClips', clips);
+            let clipsSorted = this.get('currentClipsSorted');
+            let clip = clipsSorted.get('firstObject');
+            this.set('currentClip', clip);
+            vid.pause(); // Pause the video before switching the source.
+            vid.src = clip.get('src');
+            vid.currentTime = 0;
+            vid.play();
+            // wait while clip plays
+            await this.clipEnded();
+
+            // increment clip play count
+            clip.set('playCount', clip.get('playCount') + 1);
+          }
+        }
+      }
+    }
+  },
+
+  async playPlan() {
+    // increment plan on each play
+    this.incrementPlan();
+
+    // play plan
+    this.set('playing', true);
+    let currentPlan = this.get('currentPlan');
+    await this.process(currentPlan);
+  },
+  videoReady(videoElement) {
+    this.set('video', videoElement);
+    this.playPlan();
+  },
 });
